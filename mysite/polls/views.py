b30d14ddb3fa5579eb270921bc0abcf554514e0e
Yi Zhang <yi16@cs.ualberta.ca>
2019-01-30 18:18:43 -0700

    polls:

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,9 +19,11 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -39,4 +41,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('results', args=(question.id,))) # 'polls:results'
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # 'polls:results'
